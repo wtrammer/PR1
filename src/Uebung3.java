4ada6b86a654f1406bacd5db6a501b92ea21d89a
wtrammer <wolfgang.trammer@nts.eu>
2015-10-31 09:10:01 +0100

    k

---
+++ src/Uebung3.java
@@ -0,0 +1,19 @@
+
+public class Uebung3 {
+
+	public static void main(String[] args) {
+		
+		
+		double celsius = 30.0;
+		
+		System.out.println(celsius + " degrees Celsius are "
+								+celsius2Fahrenheit(celsius) + " degrees Fahrenheit");
+		
+		double fahrenheit = 86.0;
+		
+		System.out.println(fahrenheit + " degrees Fahrenheit are "
+								+
+
+	}
+
+}
