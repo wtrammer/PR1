de1c8947c1b2b1b001e4660d815ea59228887edf
wtrammer <wolfgang.trammer@nts.eu>
2015-10-31 10:05:35 +0100

    a

--- src/Uebung3.java
+++ src/Uebung3.java
@@ -1,9 +1,34 @@
 
 public class Uebung3 {
+	
+	public static boolean isMetropolis(boolean isCapital, int inhabitants, double taxPerPersonAndMonth) {
+		
+		
+		boolean resultA = isCapital && inhabitants > 100_000;
+		
+		double totalTaxes = inhabitants * taxPerPersonAndMonth * 12;
+		
+		boolean resultB = inhabitants > 200_000 && totalTaxes > 720_000_000;
+		
+		
+	
+	
+	
+	
+	
+		
+		
+		
+		
+	
+		
+		
+	}
 
 	public static void main(String[] args) {
 		
-		System.out.println("x");
+		
+		
 		
 	}
 
