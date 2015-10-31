c34b1d885d5e5124f322b488e92d9bd049474d45
wtrammer <wolfgang.trammer@nts.eu>
2015-10-31 09:20:09 +0100

    test

--- src/Uebung3.java
+++ src/Uebung3.java
@@ -3,17 +3,8 @@ public class Uebung3 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("x");
 		
-		double celsius = 30.0;
-		
-		//System.out.println(celsius + " degrees Celsius are "
-		//						+celsius2Fahrenheit(celsius) + " degrees Fahrenheit");
-		
-		double fahrenheit = 86.0;
-		
-		//System.out.println(fahrenheit + " degrees Fahrenheit are "
-			//					+
-
 	}
 
 }
