cfdae192d1d6882c6d38a55d481f16c7493eac18
wtrammer <wolfgang.trammer@nts.eu>
2015-10-31 09:10:28 +0100

    ee

--- src/Uebung3.java
+++ src/Uebung3.java
@@ -6,13 +6,13 @@ public class Uebung3 {
 		
 		double celsius = 30.0;
 		
-		System.out.println(celsius + " degrees Celsius are "
-								+celsius2Fahrenheit(celsius) + " degrees Fahrenheit");
+		//System.out.println(celsius + " degrees Celsius are "
+		//						+celsius2Fahrenheit(celsius) + " degrees Fahrenheit");
 		
 		double fahrenheit = 86.0;
 		
-		System.out.println(fahrenheit + " degrees Fahrenheit are "
-								+
+		//System.out.println(fahrenheit + " degrees Fahrenheit are "
+			//					+
 
 	}
 
