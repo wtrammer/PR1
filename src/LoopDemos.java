c46a0206b02cbfb1c64da95fb1b27f087507c294
wtrammer <wolfgang.trammer@nts.eu>
2015-11-13 14:12:32 +0100

    d

---
+++ src/LoopDemos.java
@@ -0,0 +1,18 @@
+
+public class LoopDemos {
+
+	public static void main(String[] args) {
+
+		for(int i  =1; i <= 10; i++) {
+			System.out.println("Durchlauf "+i);
+		}
+		
+		int j = 1;
+		while(j <= 10) {
+			System.out.println("Durchlauf2 "+j);
+			j++;
+		}
+		
+	}
+
+}
